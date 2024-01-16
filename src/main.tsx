ea4a0b8d4fb0bb2a7f6c03285a04828bcd25b3bd
Mehran <mehranmf31@gmail.com>
2024-01-16 20:42:24 +0000

    feat: add main routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { CHARACTER_PAGE, MAIN_PAGE } from "./routes/app.ts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: MAIN_PAGE,
+        element: <h1>Characters</h1>,
+      },
+      {
+        path: CHARACTER_PAGE(":id"),
+        element: <h1>Character Page!</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
---
+++ src/routes/app.ts
@@ -0,0 +1,2 @@
+export const MAIN_PAGE = "/";
+export const CHARACTER_PAGE = (id: string | number) => `/characters/${id}`;
