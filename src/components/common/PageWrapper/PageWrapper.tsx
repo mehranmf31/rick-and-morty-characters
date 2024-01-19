6b80b19261d5cfda3f57430d9cfc91c69c243e82
Mehran <mehranmf31@gmail.com>
2024-01-19 21:42:05 +0000

    style: modify page wrapper spacing

--- src/components/common/PageWrapper/PageWrapper.tsx
+++ src/components/common/PageWrapper/PageWrapper.tsx
@@ -8,7 +8,7 @@ interface PageWrapperProps {
 
 export function PageWrapper({ className, children }: PageWrapperProps) {
   return (
-    <div className={classnames("container mx-auto pt-8 px-5", className)}>
+    <div className={classnames("container mx-auto pt-16 px-5", className)}>
       {children}
     </div>
   );
