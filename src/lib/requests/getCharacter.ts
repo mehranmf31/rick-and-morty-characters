93957c1ecd53f7eec8c7b396883c9413294eaba1
Mehran <mehranmf31@gmail.com>
2024-01-20 20:12:22 +0000

    fix: add type to getCharacter request

--- src/lib/requests/getCharacter.ts
+++ src/lib/requests/getCharacter.ts
@@ -1,7 +1,8 @@
 import { CHARACTER } from "@lib/constants/apiRoutes";
 import { get } from "@lib/utils/api";
+import { CharacterType } from "@types";
 
-export const getCharacter = (id: number) =>
+export const getCharacter = (id: number): Promise<CharacterType> =>
   get(CHARACTER(id))
     .then((response: any) => {
       return response;
