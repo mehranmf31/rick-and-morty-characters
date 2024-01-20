e957ce3ca346c76b982d9d1399016b9879ad002b
Mehran <mehranmf31@gmail.com>
2024-01-20 23:58:53 +0000

    feat: add character page

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,9 +1,63 @@
-import { PageWrapper } from "@components/common";
+import { useParams } from "react-router-dom";
 
-export function CharacterPage() {
+import { useCharacter } from "@lib/hooks";
+import { host } from "@lib/utils/config";
+import { EPISODES, LOCATION } from "@lib/constants/apiRoutes";
+import { LoadingWrapper, PageWrapper } from "@components/common";
+import {
+  CharacterDetails,
+  EpisodesDetails,
+  LocationDetails,
+} from "@components/characters";
+
+export const CharacterPage = (): JSX.Element => {
+  const { id } = useParams();
+  const { data, isLoading, isError, error } = useCharacter(Number(id) || 0);
+
+  if (!data || (isError && error))
+    return <div>{error?.message || "Unexpected Error!"}</div>;
+
   return (
-    <PageWrapper>
-      <h1>Main Page!</h1>
-    </PageWrapper>
+    <>
+      {isLoading ? (
+        <LoadingWrapper />
+      ) : (
+        <PageWrapper>
+          <div className="grid gap-6 mx-auto mb-16">
+            <CharacterDetails {...data} />
+
+            {!!data.origin.url && (
+              <div className="bg-white rounded-xl p-9">
+                <LocationDetails
+                  id={Number(data.origin.url.replace(host + LOCATION(""), ""))}
+                  title="Origin Details"
+                />
+              </div>
+            )}
+
+            {!!data.location.url && (
+              <div className="bg-white rounded-xl p-9">
+                <LocationDetails
+                  id={Number(
+                    data.location.url.replace(host + LOCATION(""), ""),
+                  )}
+                  title="Location Details"
+                />
+              </div>
+            )}
+
+            {!!data.episode?.length && (
+              <div className="bg-white rounded-xl p-9">
+                <EpisodesDetails
+                  list={data.episode
+                    .map((item) => item.replace(host + EPISODES(""), ""))
+                    .join(",")}
+                />
+              </div>
+            )}
+          </div>
+        </PageWrapper>
+      )}
+    </>
   );
-}
+};
