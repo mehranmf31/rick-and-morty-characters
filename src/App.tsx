893ef229dbc065f88276822f6966016f080cd27b
Mehran <mehranmf31@gmail.com>
2024-01-20 23:51:53 +0000

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { MAIN_PAGE } from "routes/app";
 
 export function App() {
   return (
     <>
+      <nav className="w-full flex justify-center pt-12">
+        <Link to={MAIN_PAGE}>
+          <img
+            src="/logo480.png"
+            alt="Rick and Morty logo"
+            className="xl:w-72"
+          />
+        </Link>
+      </nav>
       <main>
         <Outlet />
       </main>
